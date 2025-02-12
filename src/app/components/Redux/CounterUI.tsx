5b30e85e04b34dc9e53a418822981975f6e6973b
Prima-Sunil <sunil.patidar@primathon.in>
2025-02-12 15:13:05 +0530

    create alert Component

--- src/app/components/Redux/CounterUI.tsx
+++ src/app/components/Redux/CounterUI.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/redux/store';
-import { increment, decrement, incrementByAmount } from '@/redux/counterSlice';
+import { increment, decrement, incrementByAmount } from '@/redux/features/counterSlice';
 
 export default function CounterUI() {
   
