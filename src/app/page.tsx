ea7adf9a19954fe54528dc7fbf2508badfd40f4f
Prima-Sunil <sunil.patidar@primathon.in>
2025-02-12 15:22:33 +0530

    complete todo app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,14 @@
 "use client";
 
-
-// import ClickCounter from "@/app/components/UseRef/ClickCounter";
-// import ChangeBackGround from "./components/UseRef/ChangeBackGround";
-// import Timer from "./components/UseRef/Timer";
-// import MultiEffectComponents from "./components/UseEffect/MultiEffectComponents";
-
-// import ResizeComponents from "./components/UseEffect/ResizeComponents";
-
-// import LoggerComponents from "./components/UseEffect/LoggerComponents";
-// import TimerComponents from "./components/UseEffect/TimerComponents"; 
-
-// import Counter from "@/app/components/UseEffect/Counter"
-// import LoginForm from "./components/UseEffect/LoginFrom";
-// import Button from "./components/Button";
-// import Text from "./components/Text";
-// import App from "@/app/components/App";
-// import ThemeApp from "./components/ContextApi/ThemeApp";
-import CounterUI from "./components/Redux/CounterUI";
+// import CounterUI from "./components/Redux/CounterUI";
+import TodoApp from "./TodoApp/TodoApp";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-4 bg-gray-100">
-      {/* <h1 className="text-2xl font-bold">Reusable Component</h1> */}
-      {/* 
-      <Button text="Filled Button" onClick={() => alert("Filled Button Clicked")} /> */}
-      {/* <Button
-        text="Outline Button"
-        type="outline"
-        onClick={() => alert("Outline Button Clicked")}
-      />
-      <Button
-        text="Dotted Button"
-        type="dotted"
-        onClick={() => alert("Dotted Button Clicked")}
-      />
-      <Button
-        text="right"
-        type="dotted"
-        icon="right"
-        onClick={() => alert("Dotted Button Clicked")}
-      /> */}
-      {/* <Text tag="h1" >This is an H1</Text>
-      <Text tag="h2" >This is an H2</Text>
-      <Text tag="h3" color="red">This is an H3</Text>
-      <Text tag="h4">This is an H4</Text>
-      <Text tag="h5">This is an H5</Text>
-      <Text tag="h6">This is an H6</Text>
-      <Text tag="p">This is a paragraph.</Text>
-      <Text tag="span">This is a span.</Text> */}
-       {/* <Counter /> */}
-     {/* <LoggerComponents/> */}
-{/* <TimerComponents/> */}
-      {/* <LoginForm/> */}
-      {/* <ResizeComponents/> */}
-      {/* <MultiEffectComponents/> */}
-      {/* <ClickCounter/> */}
-      {/* <ChangeBackGround/> */}
-     {/* <Timer/> */}
-     {/* <ThemeApp/> */}
-    <CounterUI/>      
+    <div className="container">
+     
+      <TodoApp />
+  
     </div>
   );
 }
